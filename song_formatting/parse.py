0da269f2f258d862c44c0303dbd8a643e502f362
mstanto4 <mstanto4@vols.utk.edu>
2020-12-22 10:59:46 -0500

    continued work on parsing

--- song_formatting/parse.py
+++ song_formatting/parse.py
@@ -11,15 +11,21 @@ songContents = songData.readlines()
 
 print(songContents)
 
-for x in songContents:
+for count,x in enumerate(songContents):
 	if(x[0] == '#'):
 		text = x.split(':')
 		if(text[0] == "#BANNER" or text[0] == "#BACKGROUND" or text[0] == "#MUSIC" or text[0] == "#OFFSET"):
 			newSong.write(x)		
-		if(text[0] == "#SAMPLESTART" or text[0] == "#SAMPLELENGTH" or text[0] == "#BPMS" or text[0] == "#NOTES"):
+		if(text[0] == "#SAMPLESTART" or text[0] == "#SAMPLELENGTH" or text[0] == "#BPMS"):
 			newSong.write(x)
+		if(text[0] == "#NOTES"):
+			newSong.write(x)
+			newSong.write(songContents[count + 3])
 	elif(x == '\n'):
 		newSong.write(x)	
 	elif(x[0] == '/' and x[1] == '/'):
 		newSong.write(x)
-
+	elif(x[0] == '0' or x[1] == '1'):
+		newSong.write(x)
+	elif(x == ',\n'):
+		newSong.write(x)

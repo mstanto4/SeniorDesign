ae7664288d3e78b3db45d936b9aac252264b044c
mstanto4 <mstanto4@vols.utk.edu>
2020-12-27 12:15:02 -0500

    finished script for creating songs

--- song_formatting/parse.py
+++ song_formatting/parse.py
@@ -9,8 +9,8 @@ newSong = open(temp, 'w+')
 
 songContents = songData.readlines()
 
-print(songContents)
-
+#print(songContents)
+flag = 0
 for count,x in enumerate(songContents):
 	if(x[0] == '#'):
 		text = x.split(':')
@@ -25,7 +25,32 @@ for count,x in enumerate(songContents):
 		newSong.write(x)	
 	elif(x[0] == '/' and x[1] == '/'):
 		newSong.write(x)
-	elif(x[0] == '0' or x[1] == '1'):
+	elif(x[0] == '0' or x[0] == '1' or x[0] == '2' or x[0] == '3'):
+		ones = 0
+		for index, y in enumerate(x):
+			if(y == '1'):
+				ones = ones + 1
+			if(y == '2' or y == '3'):
+				temp = list(x)
+				temp[index] = '1'
+				x = ''.join(temp)
+			#	print(x)
+				ones = ones + 1
+				flag = 1
+		if(flag == 1 and ones < 2):
+			temp = list(x)
+			temp[4] = '1'
+			x = ''.join(temp)
+			x = x + '\n'
+#			print(x)
+			flag = 0
+		else:
+			temp = list(x)
+			temp[4] = '0'
+			x = ''.join(temp)
+			x = x + '\n'
+#			print(x)
+		#print(x)
 		newSong.write(x)
-	elif(x == ',\n'):
+	elif(x[0] == ','):
 		newSong.write(x)

262d7da897901f5ade5489a264f9121203c45d7a
Alex Twilla <alexandertwilla@gmail.com>
2021-01-22 16:09:56 -0500

    Change testing to observe rewards given.

--- app_env/env_testing.py
+++ app_env/env_testing.py
@@ -1,10 +1,12 @@
 import rhythm_game_env
 import numpy as np
 
-rg = rhythm_game_env.RhythmGameEnv(song_file="test.smm", diff="Easy")
+rg = rhythm_game_env.RhythmGameEnv(song_file="test.smm", diff="Medium")
 
 print("Number of measures:", len(rg.measure_list))
 print(rg.bpms)
+print(rg.bpm_beats)
+print(rg.bpm_steps)
 
 done = False
 score = 0
@@ -14,8 +16,19 @@ blank_note = [False for x in range(5)]
 action = blank_note
 
 while not done:
+	prev_state = state
 	done, reward, state = rg.step(action)
 	score += reward
+
+	if reward < 0:
+		print("Action:", action)
+		print("State:", prev_state)
+		print("Reward:", reward)
+
+		if action != list(prev_state[0]):
+			print("Mismatch detected.")
+
+		print()
 
 	if state[1] <= rg.perfect_threshold:
 		action = list(state[0])
@@ -23,12 +36,12 @@ while not done:
 	else:
 		action = blank_note
 
-	
-	print("Current beat:", rg.curr_beat)
-	print("Current measure:", rg.curr_measure)
-	print("Current BPM:", rg.curr_bpm)
-	print("Current step:", rg.num_steps)
 
+"""	print("Current beat:", rg.curr_beat)
+	print("Current measure:", rg.curr_measure)
+	print("Current BPM:", rg.curr_bpm) 
+	print("Current step:", rg.num_steps)
+"""
 print("Total Score:", score)
 print("Done after", rg.num_steps, "steps.")
 print("Current beat:", rg.curr_beat)

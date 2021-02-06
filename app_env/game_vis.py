92e00aba1a129c72fbed7deafabb804192e3bab0
mstanto4 <mstanto4@vols.utk.edu>
2021-02-06 14:30:04 -0500

    display note

--- app_env/game_vis.py
+++ app_env/game_vis.py
@@ -6,12 +6,22 @@ pyglet.resource.reindex()
 image = pyglet.resource.image('testBackground.jpg')
 window = pyglet.window.Window(width = image.width, height = image.height)
 
+#note height = 70 width = 94
+note_image = pyglet.resource.image('testNote.png')
+notes = []
+notes.append(pyglet.sprite.Sprite(note_image, x = 290, y = 350))
+notes.append(pyglet.sprite.Sprite(note_image, x = 510, y = 350))
+notes.append(pyglet.sprite.Sprite(note_image, x = 700, y = 350))
+notes.append(pyglet.sprite.Sprite(note_image, x = 875, y = 350))
+notes.append(pyglet.sprite.Sprite(note_image, x = 1075, y = 350))
 
 
 @window.event
 def on_draw():
 	window.clear()
 	image.blit(0,0)
+	for note in notes:
+		note.draw()
 
 pyglet.app.run()
 

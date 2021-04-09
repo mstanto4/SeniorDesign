03ea55160a975bec9c39e296d6399769d5d78743
mstanto4 <mstanto4@vols.utk.edu>
2021-04-09 10:40:37 -0400

    fixed input for game

--- app_env/game_vis_desktop.py
+++ app_env/game_vis_desktop.py
@@ -72,7 +72,9 @@ class GameState():
 				player.play()
 				self.start = True	
 		else:	
-			self.gameOver, self.reward, self.state = self.rg.step(self.action)
+			action = sum(2**i for i, v in enumerate(reversed(self.action)) if v)	
+
+			self.gameOver, self.reward, self.state = self.rg.step(action)
 			self.score += self.reward
 			self.action = [False for x in range(5)]
 			

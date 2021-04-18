248089d5d22bab3b287e323d1354d78bbe8010bc
Alex Twilla <atwilla@vols.utk.edu>
2021-04-18 10:27:26 -0500

    Reinstate network_efficacy approach.

--- training/eons_wrap_env.py
+++ training/eons_wrap_env.py
@@ -9,11 +9,12 @@ import numpy as np
 
 class EONSWrapperEnv:
 
-	def __init__(self, params_file=None, song_file="test.smm", diff="Easy"):
+	def __init__(self, params_file=None, song_file="test.smm", diff="Easy", net_efficacy=1):
 		self.rhythm_game_env = RhythmGameEnv(params_file, song_file, diff)
 		self.observation_space = spaces.Box(low=np.array([0.0]), high=np.array([1.0]))
+		self.net_efficacy = net_efficacy
 		self.action_space = spaces.Discrete(2)
-		self.prev_note = 0
+		self.saved_note = 0
 
 	def step(self, action):
 		assert (action in self.action_space), "Action must be 0 or 1."
@@ -21,16 +22,33 @@ class EONSWrapperEnv:
 		note = 0
 
 		if action == 1:
-			note = self.prev_note
+			note = self.saved_note
 
 		state, reward, done, info = self.rhythm_game_env.step(note)
 		# Stash note for later use
-		self.prev_note = state[0]
-		state[-1] /= self.rhythm_game_env.track_length
+		self.saved_note = state[0]
+		wrapper_state = 0
 
-		assert (state[-1:] in self.observation_space), "State must be between 0 and 1."
+		# Efficacy determines at what point network receives "play" signal.
+		if self.net_efficacy == 1:
 
-		return state[-1:], reward, done, info
+			if state[-1] <= self.rhythm_game_env.okay_threshold:
+				wrapper_state = 1
+
+		elif self.net_efficacy == 2:
+
+			if state[-1] <= self.rhythm_game_env.great_threshold:
+				wrapper_state = 1
+
+		else:
+			
+			if state[-1] <= self.rhythm_game_env.perfect_threshold:
+				wrapper_state = 1
+			
+
+		assert ([wrapper_state] in self.observation_space), "State must be between 0 and 1."
+
+		return [wrapper_state], reward, done, info
 
 
 	def seed(self, seed):
@@ -39,7 +57,7 @@ class EONSWrapperEnv:
 	
 	def reset(self):
 		self.rhythm_game_env.reset()
-		return np.array([0.0])
+		return np.array([0])
 
 	
 	def close(self):
